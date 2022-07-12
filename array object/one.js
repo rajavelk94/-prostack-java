4f62607552feb1e8510b0a469e66ee20bf8f5f6d
Rajavelkandasamy <108261403+Rajavelkandasamy@users.noreply.github.com>
2022-07-12 12:18:49 +0530

    finish

---
+++ array object/one.js
@@ -0,0 +1,7 @@
+let a =[ 10,20,30,40]
+console.log (a) // 10,20,30,40
+ let r1 = a.unshift(60)
+console.log (a) // 60,10,20,30,40
+console.log(a.length) // 5
+let r2 = a.shift()
+console.log(a)// 10,20,30,40
--- data types/if.js
+++ data types/if.js
@@ -1,8 +1,8 @@
-let num = 147
-if( num % 2 == 0){
+let num = 144
+if( num %2 == 0){
     console.log( "yes even");
 
 }
 else{
-    console.log("no odd")
+    console.log("yes odd")
 }
